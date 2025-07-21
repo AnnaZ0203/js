3ecbfdeae070672bbccb1abb5a1c94e50de728c3
AnnaZ0203 <annazhoga0203@gmail.com>
2025-07-21 16:09:29 +0100

    hw

---
+++ main3-1.js
@@ -0,0 +1,23 @@
+//task1
+
+let result = 5 + 5 + "5";
+console.log(result);
+console.log(typeof result);
+
+//task2
+let email = "lalalalala0203@gmail.com";
+console.log(email.includes("@"));
+console.log(email.length);
+
+//task3
+let first = "My";
+let second = "name";
+let third = "is";
+let fullName = first + second + third;
+fullName += "Victor";
+console.log(fullName);
+
+//task4
+let userName = "Anna";
+let payment = 100;
+alert(`дякуємо, ${userName}! до cплати 100 грн ;)`);
