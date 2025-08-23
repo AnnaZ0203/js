5bd7b95d1cc8da083ccc3256b4e7914e07024a19
AnnaZ0203 <annazhoga0203@gmail.com>
2025-08-23 15:18:35 +0100

    hello

---
+++ main6-1.js
@@ -0,0 +1,33 @@
+//цикли
+// task 1
+let i = 0;
+while (i <= 9) {
+  i++;
+  console.log(i);
+}
+
+//task 2
+for (let i = 2; i <= 20; i + 2) {
+  console.log(i);
+}
+
+//task 3
+for (let i = 1; i <= 10; i++) {
+  console.log(`7* ${i} = ${7 * i}`);
+}
+
+//task 4
+let n = 15;
+for (let i = 1; i <= 99; i++) {
+  if (i >= n) break;
+  console.log(i);
+}
+
+//task 5
+let m = 1;
+while (m <= 20) {
+  if (m === 0) {
+  }
+}
+
+//i dont know how to do this one :(((
